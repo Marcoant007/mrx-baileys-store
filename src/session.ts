3dd820930fcfc471f0c2a53fbece1fc3ed56f9e6
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2023-09-11 15:55:14 -0300

    update new fixes

--- src/session.ts
+++ src/session.ts
@@ -22,72 +22,75 @@ export async function useSession(sessionId: string) {
         where: { sessionId_id: { id, sessionId } },
       });
     } catch (e) {
-      logger.error(e, 'An error occured during session write');
-    }
-  };
-
-  const read = async (id: string) => {
-    try {
-      const { data } = await model.findUniqueOrThrow({
-        select: { data: true },
-        where: { sessionId_id: { id: fixId(id), sessionId } },
-      });
-      return JSON.parse(data, BufferJSON.reviver);
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError && e.code === 'P2025') {
-        logger.info({ id }, 'Trying to read non existent session data');
-      } else {
-        logger.error(e, 'An error occured during session read');
+      if (e instanceof PrismaClientKnownRequestError && e.code === 'P2000') {
+        console.error('Os dados fornecidos são muito longos para a coluna.');
       }
-      return null;
-    }
-  };
+      logger.error(e, 'An error occured during session write');
+    };
 
-  const del = async (id: string) => {
-    try {
-      await model.delete({
-        select: { pkId: true },
-        where: { sessionId_id: { id: fixId(id), sessionId } },
-      });
-    } catch (e) {
-      logger.error(e, 'An error occured during session delete');
-    }
-  };
-  //  const creds: AuthenticationCreds = (await read('creds')) || initAuthCreds();
-  const creds: AuthenticationCreds = (await model.findUnique({ where:{ sessionId_id: { id: 'creds', sessionId } }})) ? (await read('creds'))  : initAuthCreds();
+    const read = async (id: string) => {
+      try {
+        const { data } = await model.findUniqueOrThrow({
+          select: { data: true },
+          where: { sessionId_id: { id: fixId(id), sessionId } },
+        });
+        return JSON.parse(data, BufferJSON.reviver);
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError && e.code === 'P2025') {
+          logger.info({ id }, 'Trying to read non existent session data');
+        } else {
+          logger.error(e, 'An error occured during session read');
+        }
+        return null;
+      }
+    };
+
+    const del = async (id: string) => {
+      try {
+        await model.delete({
+          select: { pkId: true },
+          where: { sessionId_id: { id: fixId(id), sessionId } },
+        });
+      } catch (e) {
+        logger.error(e, 'An error occured during session delete');
+      }
+    };
+    //  const creds: AuthenticationCreds = (await read('creds')) || initAuthCreds();
+    const creds: AuthenticationCreds = (await model.findUnique({ where: { sessionId_id: { id: 'creds', sessionId } } })) ? (await read('creds')) : initAuthCreds();
 
     return {
-    state: {
-      creds,
-      keys: {
-        get: async <T extends keyof SignalDataTypeMap>(type: T, ids: string[]): Promise<{ [id: string]: SignalDataTypeMap[T] }> => {
-          const data: { [id: string]: SignalDataTypeMap[T] } = {} as any;
-          await Promise.all(
-            ids.map(async (id) => {
-              let value = await read(`${type}-${id}`);
-              if (type === 'app-state-sync-key' && value) {
-                value = proto.Message.AppStateSyncKeyData.fromObject(value);
-              }
-              data[id] = value as SignalDataTypeMap[T];
-            })
-          );
-          return data;
-        },
-        
-        set: async (data: any) => {
-          const tasks: Promise<void>[] = [];
+      state: {
+        creds,
+        keys: {
+          get: async <T extends keyof SignalDataTypeMap>(type: T, ids: string[]): Promise<{ [id: string]: SignalDataTypeMap[T] }> => {
+            const data: { [id: string]: SignalDataTypeMap[T] } = {} as any;
+            await Promise.all(
+              ids.map(async (id) => {
+                let value = await read(`${type}-${id}`);
+                if (type === 'app-state-sync-key' && value) {
+                  value = proto.Message.AppStateSyncKeyData.fromObject(value);
+                }
+                data[id] = value as SignalDataTypeMap[T];
+              })
+            );
+            return data;
+          },
 
-          for (const category in data) {
-            for (const id in data[category]) {
-              const value = data[category][id];
-              const sId = `${category}-${id}`;
-              tasks.push(value ? write(value, sId) : del(sId));
+          set: async (data: any) => {
+            const tasks: Promise<void>[] = [];
+
+            for (const category in data) {
+              for (const id in data[category]) {
+                const value = data[category][id];
+                const sId = `${category}-${id}`;
+                tasks.push(value ? write(value, sId).then(() => {}) : del(sId).then(() => {}));
+              }
             }
-          }
-          await Promise.all(tasks);
+            await Promise.all(tasks);
+          },
         },
       },
-    },
-    saveCreds: () => write(creds, 'creds'),
-  };
-}
+      saveCreds: () => write(creds, 'creds'),
+    };
+  }
+  }
